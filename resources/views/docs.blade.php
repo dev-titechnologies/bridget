9010a22a23d3274b550d1382ad0a3bd1c882230e
nithinkumar <nithinkumar.cn@titechnologies.in>
2018-02-21 16:55:29 +0530

    docs

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -7,8 +7,8 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
 
-            {!! '<div id="bridget_container"></div>
-                <script src="http://ec2-54-252-171-131.ap-southeast-2.compute.amazonaws.com/js/bridget_maybe.js"></script>' !!}
+            {!! <div id="bridget_container"></div>
+                <script src="http://ec2-54-252-171-131.ap-southeast-2.compute.amazonaws.com/js/bridget_maybe.js"></script> !!}
 
              
  
