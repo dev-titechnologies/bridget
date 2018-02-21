11282f277c035a2c50c51a2fc60fedfe14bc5536
nithinkumar <nithinkumar.cn@titechnologies.in>
2018-02-21 16:41:54 +0530

    docs

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -6,10 +6,11 @@
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
-            echo HTML::entities('
-                <div id="bridget_container"></div>
-                <script src="http://ec2-54-252-171-131.ap-southeast-2.compute.amazonaws.com/js/bridget_maybe.js"></script>
-            ');
+
+            {!! '<div id="bridget_container"></div>
+                <script src="http://ec2-54-252-171-131.ap-southeast-2.compute.amazonaws.com/js/bridget_maybe.js"></script>''' !!}
+
+             
  
 
             </div>
