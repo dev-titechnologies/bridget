0cf62b17b2a32bd2573d547ac750eaad0123c57c
nithinkumar <nithinkumar.cn@titechnologies.in>
2018-02-21 16:25:55 +0530

    docs

---
+++ resources/views/docs.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+       
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                 
+
+                <code>
+
+                <div id="bridget_container"></div>
+                <script src="http://ec2-54-252-171-131.ap-southeast-2.compute.amazonaws.com/js/bridget_maybe.js"></script>
+
+                <code>
+
+
+
+            </div>
+        </div>
+    </body>
+</html>
