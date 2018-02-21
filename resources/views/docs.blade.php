94d8a62bf797013cf884384cf0e60dc86b3065c6
nithinkumar <nithinkumar.cn@titechnologies.in>
2018-02-21 17:52:46 +0530

    docs

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,14 +1,18 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-       
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
 
-            {!! <div id="bridget_container"></div>
-                <script src="http://ec2-54-252-171-131.ap-southeast-2.compute.amazonaws.com/js/bridget_maybe.js"></script> !!}
+            <pre class="prettyprint">
+                 <div id="bridget_container"></div>
+                <script src="http://ec2-54-252-171-131.ap-southeast-2.compute.amazonaws.com/js/bridget_maybe.js"></script>
+                
+                </pre>
 
              
  
