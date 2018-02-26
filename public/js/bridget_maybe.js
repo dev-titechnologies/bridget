887cab9c0f5f2c4a7e3eaec91103d7a0610654b2
Soju Varughese Thomas <soju.varughese@titechnologies.org>
2018-02-26 20:15:20 +0530

    bridget maybe js added

--- public/js/bridget_maybe.js
+++ public/js/bridget_maybe.js
@@ -1,133 +1,75 @@
- var assetPath ='http://192.168.1.57/bridget_backend/public';
+//------------CONFIGS------------------//
 
- function getScript(url){
- 	var script=document.createElement('script');
- 	script.src=url;
- 	var head=document.getElementsByTagName('head')[0];
- 	done=false;
- 	return new Promise(function(resolve) {
- 		script.onload=script.onreadystatechange = function(){
- 			if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete') ) {
- 				resolve();
- 				script.onload = script.onreadystatechange = null;
- 			}
- 		};
- 		head.appendChild(script);
- 	});
- }
+var assetPath = 'http://bridget.com/public';
+var iFrameUrl = assetPath + '/bridget';
+var ContainerId = '#bridget_container'
+var IframeContainerId = '.bridget-frame'
+//------------/CONFIGS------------------//
 
- promise1=null;
- promise2=null;
+promise1 = null;
+promise2 = null;
 
- if(typeof jQuery == 'undefined'){
- 	promise1=getScript('http://code.jquery.com/jquery.min.js');
- }
-
- Promise.all([promise1, promise2]).then(function(values) {
-       //write jquery
-       $('head').append('<link rel="stylesheet" href="'+assetPath+'/css/bridget.css" type="text/css" />');
-
-       var $html='<div class="floating-chat enter" style="display:none;">'+
-       '<i class="fa fa-comments" aria-hidden="true"></i>'+
-       '<div class="chat">'+
-       '<div class="header">'+
-       '<span class="title">'+
-       'What do you think?'+
-       '</span>'+
-       '<button  class="close">'+
-       '<i class="fa fa-times" aria-hidden="true"></i>'+
-       '</button>'+
-       '</div>'+
-       '<div id="iframe-container"></div>'+
-       '</div>'+
-       '</div>';
-       $('#bridget_container').html($html);
-
-       $(document).ready(function(){
-       	$('.floating-chat').show();
-       })
-
-       var selectedElement=null;
-
-       var domElements=(function(){
-       	return {       
-       		'replayBtn':'.replay-btn',
-       		'chatContainer':'.floating-chat',
-       		'messagesContainer':'.messages',
-       		'seeAllReplays':'.see-all-replay',
-       		'childCommentContainer':'.child_comment_container',
-       		'hideAllReplay':'.hide-all-replay',
-       		'commentFooter':'.comment_footer',
-       		'cancelReplayInput':'.cancel_input',
-       		'userReplayInput':'.user-replay',
-       		'sendMessageBtn':'#sendMessage',
-       		'messageTextBox':'.comment-box',
-       		'responseMessage':'#new-message',
-       		'anonymousPostBtn':'.anonymous-post',
-       		'userNameField':'.name_field',
-       		'botContainer':'.bot-container'
-       	}
-
-       })();
-
-
-
-
-       var element = $(domElements.chatContainer);
-
-       setTimeout(function() {
-       	element.addClass('enter');
-       	$('.floating-chat').show();
-       }, 1000);
-
-       element.click(openElement);
-
-
-       function openElement() 
-       { 
-             
-       	var messages = element.find('.messages');
-       	var textInput = element.find(domElements.messageTextBox);
-       	/*element.find('>i').hide();*/
-       	element.addClass('expand');
-       	element.find('.chat').addClass('enter');
-       	element.off('click', openElement);
-       	element.find('.header button').click(closeElement);
-       	/*messages.scrollTop(messages.prop("scrollHeight"));*/
-
-       	/*FRAME*/
-       	var iFrame = document.createElement("iframe");
-       	var url =window.location.href;
-       	var bridgetFingerprint = localStorage.getItem('bridget-fingerprint');
-       	var bridgetUsername = localStorage.getItem('bridget-username');
-       	if(!bridgetFingerprint){
-       		var bridgetFingerprint = new Date().getTime() + Math.random();
-       		localStorage.setItem("bridget-fingerprint", bridgetFingerprint);   
-       	}
-       	iFrame.setAttribute("src", assetPath+"/bridget?bridget_url="+url+"&fingerPrint="+bridgetFingerprint);
-       	iFrame.setAttribute("id", "bridget_maybe");
-       	iFrame.setAttribute("frameBorder", 0);
-       	iFrame.style.width = "100%";
-       	iFrame.style.height = "100%";
-       	document.getElementById('iframe-container').appendChild(iFrame);
-       }
-
-function closeElement() {
-	
-	var frame = document.getElementById("bridget_maybe");
-	frame.parentNode.removeChild(frame);
-	element.find('.chat').removeClass('enter').hide();
-	element.find('>i').show();
-	element.removeClass('expand');
-	element.find('.header button').off('click', closeElement);
-	setTimeout(function() {
-		element.find('.chat').removeClass('enter').show()
-		element.click(openElement);
-	}, 500);
+if (typeof jQuery == 'undefined') {
+    promise1 = getScript("http://code.jquery.com/jquery-latest.min.js",function(){
+       $('head').append('<link rel="stylesheet" href="' + assetPath + '/css/bridgit.css" type="text/css" />');
+       $('head').append('<meta name="viewport" content="width=device-width,  initial-scale=1.0, maximum-scale=1.0, user-scalable=0>');
+       $(ContainerId).html('<div class="bridget-bridget-chat-btn active"> <div class="bridget-floating-bridget-chat enter">  <img src="' + assetPath + '/img/comment.png" width="25"> </div> </div>');
+       $(ContainerId).append('<section class="bridgit-messenger"> <div class="bridget-menu"> <div class="button">...</div> </div> <div class="bridget-chat"> <div class="bridget-chat-title"> <h1> What do you think? </h2> </div> <div class="bridget-frame">Loading... </div>   </div> </section>');
+       promiseFunctions();
+       addIframe()
+   });
 
 }
 
-});
+function getScript(url,cb) {
+    var script = document.createElement('script');
+    script.src = url;
+    var head = document.getElementsByTagName('head')[0];
+    done = false;
+    head.appendChild(script);
+
+    script.onload = script.onreadystatechange = function () {
+        if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+            cb();
+            script.onload = script.onreadystatechange = null;
+        }
+    };
+    head.appendChild(script);
+
+}
 
 
- 
+function promiseFunctions() {
+
+
+    $('.bridget-bridget-chat-btn').click(function () {
+        $('.bridget-bridget-chat-btn').toggleClass('active');
+        $('.bridgit-messenger').toggleClass('active');
+    });
+
+    $('.bridget-menu').click(function () {
+        $('.bridget-bridget-chat-btn').toggleClass('active');
+        $('.bridgit-messenger').toggleClass('active');
+    });
+
+
+
+}
+function addIframe() {
+    var bridgetFingerprint = localStorage.getItem('bridget-fingerprint');
+    if(!bridgetFingerprint){
+        var bridgetFingerprint = new Date().getTime() + Math.random();
+        localStorage.setItem("bridget-fingerprint", bridgetFingerprint);   
+    }
+    var url =window.location.href;
+    iFrameUrl=iFrameUrl+'?bridget_url='+url+'&fingerPrint='+bridgetFingerprint;
+    var $frame = $('<iframe style="width:100%; height:100%;" src="' + iFrameUrl + '" frameborder="0">');
+    $(IframeContainerId).html($frame);
+}
+
+
+
+
+
+
+
